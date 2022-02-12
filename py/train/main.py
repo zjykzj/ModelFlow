97a6cc148f8350a3daaa46a93becff0b2b2fe8db
zjkjzj <wy163zhuj@163.com>
2022-02-12 14:19:38 +0800

    refactor(train): separate model definition from main.py

--- py/train/main.py
+++ py/train/main.py
@@ -1,14 +1,21 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import print_function
 import argparse
 import os.path
 
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
+# from torchvision.models.mobilenet import mobilenet_v2
 
+from tiny_net import Net
+
+"""
+Remember: MNIST image size is (1, 28, 28)
+"""
 
 def load_args():
     # Training settings
@@ -34,32 +41,6 @@ def load_args():
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     return parser.parse_args()
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
@@ -125,6 +106,7 @@ def main():
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     model = Net().to(device)
+    # model = mobilenet_v2(num_classes=10).to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
