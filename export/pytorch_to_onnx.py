586312610d809a551155f2ccec201d201bf25481
zjkjzj <wy163zhuj@163.com>
2023-10-29 14:51:18 +0800

    perf(pytorch_to_onnx.py): support Dynamic axes setting

--- export/pytorch_to_onnx.py
+++ export/pytorch_to_onnx.py
@@ -15,6 +15,9 @@ Usage: Convert Pytorch ToyNet to ONNX:
 Usage: Convert Pytorch Resnet18 to ONNX:
     $ python pytorch_to_onnx.py --model resnet18 --save resnet18_pytorch.onnx
 
+Usage: Dynamically setting batch size:
+    $ python pytorch_to_onnx.py --model resnet18 --save resnet18_pytorch.onnx --dynamic
+
 """
 
 import argparse
@@ -36,6 +39,8 @@ def parse_opt():
                         help="Pytorch Model path, default: None, means using ToyNet")
     parser.add_argument("--save", metavar="SAVE", type=str, default='../assets/mnist_cnn.onnx',
                         help="Saving onnx path, default: '../assets/mnist_cnn.onnx'")
+    parser.add_argument("--dynamic", action="store_true", default=False,
+                        help="Is the batch dimension dynamically set, default: False")
 
     args = parser.parse_args()
     print(f"args: {args}")
@@ -68,7 +73,7 @@ def check_output(x, torch_out, onnx_path='pytorch.onnx'):
     # Since ORT 1.9, you are required to explicitly set the providers parameter when instantiating InferenceSession.
     # For example, onnxruntime.InferenceSession(..., providers=['TensorrtExecutionProvider', 'CUDAExecutionProvider', 'CPUExecutionProvider'], ...)
     ort_session = onnxruntime.InferenceSession(onnx_path, providers=['CPUExecutionProvider'])
-    print("onnx info:")
+    print("Onnx info:")
     print(f"    input: {ort_session.get_inputs()[0]}")
     print(f"    output: {ort_session.get_outputs()[0]}")
 
@@ -78,6 +83,7 @@ def check_output(x, torch_out, onnx_path='pytorch.onnx'):
     # compute ONNX Runtime output prediction
     ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
     ort_outs = ort_session.run(None, ort_inputs)
+    print(x.shape, ort_outs[0].shape)
 
     # compare ONNX Runtime and PyTorch results
     np.testing.assert_allclose(to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=1e-05)
@@ -86,6 +92,10 @@ def check_output(x, torch_out, onnx_path='pytorch.onnx'):
 
 def export_to_onnx(torch_model, batch_size=1, img_dim=1, img_size=28, onnx_path="pytorch.onnx", is_dynamic=False):
     assert isinstance(torch_model, nn.Module)
+
+    if is_dynamic:
+        batch_size = 5
+
     # Input to the model
     x = torch.randn(batch_size, img_dim, img_size, img_size, requires_grad=True)
     torch_out = torch_model(x)
@@ -130,7 +140,9 @@ def main(args):
         img_size = 224
 
     onnx_path = args.save
-    export_to_onnx(model, batch_size=batch_size, img_dim=img_dim, img_size=img_size, onnx_path=onnx_path)
+    is_dynamic = args.dynamic
+    export_to_onnx(model, batch_size=batch_size, img_dim=img_dim, img_size=img_size,
+                   onnx_path=onnx_path, is_dynamic=is_dynamic)
     print(f"Save to {onnx_path}")
 
 
