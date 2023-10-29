6722859770ecb233c5648a4da6d272292f28f967
zjykzj <wy163zhuj@163.com>
2023-10-29 18:52:48 +0800

    perf(export/onnx_to_engine.py): check ONNXRuntime and TensorRT output

--- export/onnx_to_engine.py
+++ export/onnx_to_engine.py
@@ -8,6 +8,7 @@
 """
 
 import argparse
+import onnxruntime
 
 import numpy as np
 import tensorrt as trt
@@ -35,7 +36,7 @@ def parse_opt():
 
 def convert_onnx_to_engine(onnx_filename, engine_filename=None, max_batch_size=32, max_workspace_size=1 << 30,
                            fp16_mode=True):
-    # In node -1 (importModel): INVALID_VALUE: Assertion failed: !_importer_ctx.network()->hasImplicitBatchDimension() && "This version of the ONNX parser only supports TensorRT INetworkDefinitions with an explicit batch dimension. Please ensure the network was created using the EXPLICIT_BATCH NetworkDefinitionCreationFlag."
+    # ERROR: In node -1 (importModel): INVALID_VALUE: Assertion failed: !_importer_ctx.network()->hasImplicitBatchDimension() && "This version of the ONNX parser only supports TensorRT INetworkDefinitions with an explicit batch dimension. Please ensure the network was created using the EXPLICIT_BATCH NetworkDefinitionCreationFlag."
     # Fix: https://blog.csdn.net/blueblood7/article/details/123697205
     EXPLICIT_BATCH = 1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
     logger = trt.Logger(trt.Logger.WARNING)
@@ -62,14 +63,96 @@ def convert_onnx_to_engine(onnx_filename, engine_filename=None, max_batch_size=3
                 return engine, logger
 
 
+class ONNXClassifierWrapper():
+    def __init__(self, file, num_classes, target_dtype=np.float32):
+        self.target_dtype = target_dtype
+        self.num_classes = num_classes
+        self.load(file)
+
+        self.stream = None
+
+    def load(self, file):
+        f = open(file, "rb")
+        runtime = trt.Runtime(trt.Logger(trt.Logger.WARNING))
+
+        engine = runtime.deserialize_cuda_engine(f.read())
+        self.context = engine.create_execution_context()
+
+    def allocate_memory(self, batch):
+        self.output = np.empty(self.num_classes,
+                               dtype=self.target_dtype)  # Need to set both input and output precisions to FP16 to fully enable FP16
+
+        # Allocate device memory
+        self.d_input = cuda.mem_alloc(1 * batch.nbytes)
+        self.d_output = cuda.mem_alloc(1 * self.output.nbytes)
+
+        self.bindings = [int(self.d_input), int(self.d_output)]
+
+        self.stream = cuda.Stream()
+
+    def predict(self, batch):  # result gets copied into output
+        if self.stream is None:
+            self.allocate_memory(batch)
+
+        # Transfer input data to device
+        cuda.memcpy_htod_async(self.d_input, batch, self.stream)
+        # Execute model
+        self.context.execute_async_v2(self.bindings, self.stream.handle, None)
+        # Transfer predictions back
+        cuda.memcpy_dtoh_async(self.output, self.d_output, self.stream)
+        # Syncronize threads
+        self.stream.synchronize()
+
+        return self.output
+
+
+def get_onnx_output(onnx_path):
+    ort_session = onnxruntime.InferenceSession(onnx_path,
+                                               providers=['TensorrtExecutionProvider', 'CUDAExecutionProvider',
+                                                          'AzureExecutionProvider', 'CPUExecutionProvider'])
+    print("Onnx info:")
+    print(f"    input: {ort_session.get_inputs()[0]}")
+    print(f"    output: {ort_session.get_outputs()[0]}")
+
+    assert isinstance(ort_session.get_inputs()[0].shape[0], int)
+    np.random.seed(10)
+    x = np.random.randn(*ort_session.get_inputs()[0].shape).astype(np.float32)
+    num_classes = ort_session.get_outputs()[0].shape[1]
+
+    # compute ONNX Runtime output prediction
+    ort_inputs = {ort_session.get_inputs()[0].name: x}
+    ort_outs = ort_session.run(None, ort_inputs)
+    print(x.shape, ort_outs[0].shape)
+
+    return x, num_classes, ort_outs[0]
+
+
+def check_engine(x, num_classes, ort_out, engine_path, is_fp16=False):
+    target_type = np.float16 if is_fp16 else np.float32
+    engine_model = ONNXClassifierWrapper(engine_path, num_classes, target_dtype=target_type)
+
+    trt_out = engine_model.predict(x.astype(target_type)).reshape(ort_out.shape)
+
+    # compare ONNX Runtime and TensorRT results
+    np.testing.assert_allclose(ort_out, trt_out, rtol=1e-02, atol=1e-02)
+    print("Exported model has been tested with ONNXRuntime and TensorRT, and the result looks good!")
+
+
 def main(args):
     onnx_path = args.onnx
     engine_path = args.engine
     batch_size = args.batch_size
     use_fp16 = args.fp16
 
+    print("=> convert_onnx_to_engine")
     convert_onnx_to_engine(onnx_path, engine_path, max_batch_size=batch_size, fp16_mode=use_fp16)
     print(f"Save engine to {engine_path}")
+
+    if not use_fp16:
+        print("=> get_onnx_output")
+        x, num_classes, ort_out = get_onnx_output(onnx_path)
+        print("=> check_engine")
+        check_engine(x, num_classes, ort_out, engine_path, is_fp16=use_fp16)
 
 
 if __name__ == '__main__':
