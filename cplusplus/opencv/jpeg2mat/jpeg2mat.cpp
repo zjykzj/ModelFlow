21af2c8b40b5a0e59f620cb0c1da176c71a1a4c9
zjkjzj <wy163zhuj@163.com>
2024-05-01 14:01:02 +0800

    perf(jpeg2mat): udpate cplusplus/opencv/jpeg2mat

---
+++ cplusplus/opencv/jpeg2mat/jpeg2mat.cpp
@@ -0,0 +1,27 @@
+//
+// Created by zj on 24-5-1.
+//
+
+
+
+#include "jpeg2mat.h"
+
+int Mat2Jpeg(const cv::Mat &mat_data, std::vector<unsigned char> &jpeg_buff) {
+    std::vector<int> param;
+    param.push_back(cv::IMWRITE_JPEG_QUALITY);
+    param.push_back(95);// default(95) 0-100
+    cv::imencode(".jpg", mat_data, jpeg_buff, param);
+
+    return 0;
+}
+
+int Jpeg2Mat(const std::vector<unsigned char> &jpeg_buff, cv::Mat &mat_data) {
+    if (jpeg_buff[0] == 0xFF && jpeg_buff[1] == 0xD8)
+        mat_data = cv::imdecode(jpeg_buff, cv::ImreadModes::IMREAD_COLOR);
+    else {
+        std::cerr << "jpeg_buff format error" << std::endl;
+        exit(1);
+    }
+
+    return 0;
+}
---
+++ cplusplus/opencv/jpeg2mat/jpeg2mat.h
@@ -0,0 +1,15 @@
+//
+// Created by zj on 24-5-1.
+//
+
+#ifndef OPENCV_JPEG2MAT_JPET2MAT_H_
+#define OPENCV_JPEG2MAT_JPET2MAT_H_
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int Mat2Jpeg(const cv::Mat &mat_data, std::vector<unsigned char> &jpeg_buff);
+
+int Jpeg2Mat(const std::vector<unsigned char> &jpeg_buff, cv::Mat &mat_data);
+
+#endif//OPENCV_JPEG2MAT_JPET2MAT_H_
