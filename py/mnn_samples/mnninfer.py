0fadb3b647f1083d8bdb4333d1fb7f0434ab4e6d
zjkjzj <wy163zhuj@163.com>
2024-05-01 16:18:29 +0800

    style(mnninfer.py): update

--- py/mnn_samples/mnninfer.py
+++ py/mnn_samples/mnninfer.py
@@ -13,7 +13,7 @@ import MNN.numpy as np
 import MNN.expr as expr
 
 # 创建interpreter
-interpreter = MNN.Interpreter("mobilenet_v1.mnn_samples")
+interpreter = MNN.Interpreter("mobilenet_v1.mnn")
 # 创建session
 config = {}
 config['precision'] = 'low'
@@ -36,8 +36,9 @@ image_processer = MNN.CVImageProcess({'sourceFormat': MNN.CV_ImageFormat_BGR,
                                       'normal': (0.017, 0.017, 0.017, 0.0)})
 image_data = image.ptr
 src_height, src_width, channel = image.shape
-input_data1 = MNN.Tensor((1, dst_height, dst_width, channel), MNN.Halide_Type_Float, MNN.Tensor_DimensionType_Tensorflow)
-#设置图像变换矩阵
+input_data1 = MNN.Tensor((1, dst_height, dst_width, channel), MNN.Halide_Type_Float,
+                         MNN.Tensor_DimensionType_Tensorflow)
+# 设置图像变换矩阵
 matrix = MNN.CVMatrix()
 x_scale = src_width / dst_width
 y_scale = src_height / dst_height
@@ -48,12 +49,12 @@ image_processer.convert(image_data, src_width, src_height, 0, input_data1)
 # 使用cv模块处理第二张图片，将图片转换为转换为size=(224, 224), dtype=float32，并赋值给input_data2
 image = cv.imread('TestMe.jpg')
 image = cv.resize(image, (224, 224), mean=[103.94, 116.78, 123.68], norm=[0.017, 0.017, 0.017])
-input_data2 = np.expand_dims(image, 0) # [224, 224, 3] -> [1, 224, 224, 3]
+input_data2 = np.expand_dims(image, 0)  # [224, 224, 3] -> [1, 224, 224, 3]
 
 # 合并2张图片到，并赋值给input_data
-input_data1 = expr.const(input_data1.getHost(), input_data1.getShape(), expr.NHWC) # Tensor -> Var
+input_data1 = expr.const(input_data1.getHost(), input_data1.getShape(), expr.NHWC)  # Tensor -> Var
 input_data = np.concatenate([input_data1, input_data2])  # [2, 224, 224, 3]
-input_data = MNN.Tensor(input_data) # Var -> Tensor
+input_data = MNN.Tensor(input_data)  # Var -> Tensor
 
 # 演示多张图片输入，所以将输入resize到[2, 3, 224, 224]
 interpreter.resizeTensor(input_tensor, (2, 3, 224, 224))
