c4b237bb6c384a915362b79a4dbfaff3c2471bdf
zjkjzj <wy163zhuj@163.com>
2024-09-07 21:47:47 +0800

    perf(tesnorrt): module yolov5 and tensorrt_v7.x/v8.x

--- py/yolov5/yolov5_trt_w_numpy.py
+++ py/yolov5/yolov5_trt_w_numpy.py
@@ -6,13 +6,6 @@
 @Author  : zj
 @Description:
 
-# Start Docker Container
->>>docker run --gpus all -it --rm -v ${PWD}:/workdir --workdir=/workdir ultralytics/yolov5:latest bash
-# Convert onnx to engine
->>>trtexec --onnx=yolov5s.onnx --saveEngine=yolov5s.engine
-# Install pycuda
->>>pip3 install pycuda -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
-
 Usage: Infer Image/Video using YOLOv5 with TensorRT and Numpy:
     $ python3 py/yolov5/yolov5_trt_w_numpy.py yolov5s.engine assets/bus.jpg
     $ python3 py/yolov5/yolov5_trt_w_numpy.py yolov5s.engine assets/vtest.avi --video
@@ -23,80 +16,22 @@ Usage: Save Image/Video:
 
 """
 
-import os
-
-import numpy as np
 from numpy import ndarray
 
-import tensorrt as trt
-import pycuda.autoinit
-import pycuda.driver as cuda
+from yolov5_base import YOLOv5Base
 
 from general import LOGGER
-from yolov5_base import YOLOv5Base
 
 
 class YOLOv5TRT(YOLOv5Base):
 
     def __init__(self, weight: str = 'yolov5s.engine'):
         super().__init__()
-        self.load_engine(weight)
 
-    def load_engine(self, weight: str):
-        assert os.path.isfile(weight), weight
-        LOGGER.info(f'Loading {weight} for TensorRT inference...')
-
-        TRT_LOGGER = trt.Logger(trt.Logger.INFO)
-        with open(weight, 'rb') as f, trt.Runtime(TRT_LOGGER) as runtime:
-            engine = runtime.deserialize_cuda_engine(f.read())
-        self.context = engine.create_execution_context()
-        self.stream = cuda.Stream()
-
-        # Allocate memory
-        self.inputs, self.outputs, self.bindings, self.output_shapes = [], [], [], []
-        for binding in engine:
-            size = trt.volume(engine.get_binding_shape(binding))
-            dtype = trt.nptype(engine.get_binding_dtype(binding))
-            # Allocate host and device buffers
-            host_mem = cuda.pagelocked_empty(size, dtype)
-            device_mem = cuda.mem_alloc(host_mem.nbytes)
-            # Append the device buffer to device bindings.
-            self.bindings.append(int(device_mem))
-            LOGGER.info(f"{binding} {engine.get_binding_shape(binding)}")
-            # Append to the appropriate list.
-            if engine.binding_is_input(binding):
-                self.input_w = engine.get_binding_shape(binding)[-1]
-                self.input_h = engine.get_binding_shape(binding)[-2]
-                self.inputs.append({'host': host_mem, 'device': device_mem})
-            else:
-                self.output_shapes.append(engine.get_binding_shape(binding))
-                self.outputs.append({'host': host_mem, 'device': device_mem})
-
-        self.dtype = np.dtype(trt.nptype(engine.get_binding_dtype(binding)))
-        LOGGER.info(f"Init Done. Work with {self.dtype}")
+        self.session = BackendTensorRT(weight)
 
     def infer(self, im: ndarray):
-        # Copy input image to host buffer
-        self.inputs[0]['host'] = np.ravel(im.astype(self.dtype))
-        # Transfer input data to the GPU.
-        for inp in self.inputs:
-            cuda.memcpy_htod_async(inp['device'], inp['host'], self.stream)
-
-        # Transfer input data to the GPU.
-        self.context.execute_async_v2(bindings=self.bindings, stream_handle=self.stream.handle)
-
-        # fetch outputs from gpu
-        for out in self.outputs:
-            cuda.memcpy_dtoh_async(out['host'], out['device'], self.stream)
-        # synchronize stream
-        self.stream.synchronize()
-
-        outputs = [out['host'] for out in self.outputs]
-        reshaped = []
-        for output, shape in zip(outputs, self.output_shapes):
-            reshaped.append(output.reshape(shape))
-
-        return reshaped
+        return self.session(im)
 
     def preprocess(self, im0, img_size=640, stride=32, auto=False):
         return super().preprocess(im0, img_size, stride, auto)
@@ -127,6 +62,8 @@ def parse_opt():
 
     parser.add_argument("--save", action="store_true", default=False,
                         help="Save or not.")
+    parser.add_argument("--v7", action='store_true', default=False,
+                        help="Use TensorRT_v8.x.x.x or TensorRT_v7.x.x.x. Defaults to TensorRT_v8.x.x.x")
 
     args = parser.parse_args()
     LOGGER.info(f"args: {args}")
@@ -144,4 +81,11 @@ def main(args):
 
 if __name__ == '__main__':
     args = parse_opt()
+    if args.v7:
+        print(f"Use TensorRT 7.x.x.x")
+        from py.backends.backend_tensorrt_7x import BackendTensorRT_7x as BackendTensorRT
+    else:
+        print(f"Use TensorRT 8.x.x.x")
+        from py.backends.backend_tensorrt_8x import BackendTensorRT_8x as BackendTensorRT
+
     main(args)
--- py/yolov5/yolov5_trt_w_torch.py
+++ py/yolov5/yolov5_trt_w_torch.py
@@ -6,13 +6,6 @@
 @Author  : zj
 @Description:
 
-# Start Docker Container
->>>docker run --gpus all -it --rm -v ${PWD}:/workdir --workdir=/workdir ultralytics/yolov5:latest bash
-# Convert onnx to engine
->>>trtexec --onnx=yolov5s.onnx --saveEngine=yolov5s.engine
-# Install pycuda
->>>pip3 install pycuda -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
-
 Usage: Infer Image/Video using YOLOv5 with TensorRT and Pytorch:
     $ python3 py/yolov5/yolov5_trt_w_torch.py yolov5s.engine assets/bus.jpg
     $ python3 py/yolov5/yolov5_trt_w_torch.py yolov5s.engine assets/vtest.avi --video
@@ -23,89 +16,31 @@ Usage: Save Image/Video:
 
 """
 
-import os
-
 import torch
 from torch import Tensor
 
 import numpy as np
 from numpy import ndarray
 
-import tensorrt as trt
-import pycuda.autoinit
-import pycuda.driver as cuda
+from yolov5_base import YOLOv5Base
 
 from general import LOGGER
 from yolov5_util import letterbox
 from torch_util import non_max_suppression, scale_boxes
-from yolov5_base import YOLOv5Base
 
 
 class YOLOv5TRT(YOLOv5Base):
 
     def __init__(self, weight: str = 'yolov5s.engine'):
         super().__init__()
-        self.load_engine(weight)
+        self.session = BackendTensorRT(weight)
         self.device = torch.device("cuda")
-
-    def load_engine(self, weight: str):
-        assert os.path.isfile(weight), weight
-        LOGGER.info(f'Loading {weight} for TensorRT inference...')
-
-        TRT_LOGGER = trt.Logger(trt.Logger.INFO)
-        with open(weight, 'rb') as f, trt.Runtime(TRT_LOGGER) as runtime:
-            engine = runtime.deserialize_cuda_engine(f.read())
-        self.context = engine.create_execution_context()
-        self.stream = cuda.Stream()
-
-        # Allocate memory
-        self.inputs, self.outputs, self.bindings, self.output_shapes = [], [], [], []
-        for binding in engine:
-            size = trt.volume(engine.get_binding_shape(binding))
-            dtype = trt.nptype(engine.get_binding_dtype(binding))
-            # Allocate host and device buffers
-            host_mem = cuda.pagelocked_empty(size, dtype)
-            device_mem = cuda.mem_alloc(host_mem.nbytes)
-            # Append the device buffer to device bindings.
-            self.bindings.append(int(device_mem))
-            # print(binding, engine.get_binding_shape(binding))
-            LOGGER.info(f"{binding} {engine.get_binding_shape(binding)}")
-            # Append to the appropriate list.
-            if engine.binding_is_input(binding):
-                self.input_w = engine.get_binding_shape(binding)[-1]
-                self.input_h = engine.get_binding_shape(binding)[-2]
-                self.inputs.append({'host': host_mem, 'device': device_mem})
-            else:
-                self.output_shapes.append(engine.get_binding_shape(binding))
-                self.outputs.append({'host': host_mem, 'device': device_mem})
-
-        self.dtype = np.dtype(trt.nptype(engine.get_binding_dtype(binding)))
-        LOGGER.info(f"Init Done. Work with {self.dtype}")
 
     def infer(self, im: Tensor):
         im = im.cpu().numpy()  # torch to numpy
 
-        # Copy input image to host buffer
-        self.inputs[0]['host'] = np.ravel(im.astype(self.dtype))
-        # Transfer input data to the GPU.
-        for inp in self.inputs:
-            cuda.memcpy_htod_async(inp['device'], inp['host'], self.stream)
+        y = self.session(im)
 
-        # Transfer input data to the GPU.
-        self.context.execute_async_v2(bindings=self.bindings, stream_handle=self.stream.handle)
-
-        # fetch outputs from gpu
-        for out in self.outputs:
-            cuda.memcpy_dtoh_async(out['host'], out['device'], self.stream)
-        # synchronize stream
-        self.stream.synchronize()
-
-        outputs = [out['host'] for out in self.outputs]
-        reshaped = []
-        for output, shape in zip(outputs, self.output_shapes):
-            reshaped.append(output.reshape(shape))
-
-        y = reshaped
         if isinstance(y, (list, tuple)):
             return self.from_numpy(y[0]) if len(y) == 1 else [self.from_numpy(x) for x in y]
         else:
@@ -171,6 +106,8 @@ def parse_opt():
 
     parser.add_argument("--save", action="store_true", default=False,
                         help="Save or not.")
+    parser.add_argument("--v7", action='store_true', default=False,
+                        help="Use TensorRT_v8.x.x.x or TensorRT_v7.x.x.x. Defaults to TensorRT_v8.x.x.x")
 
     args = parser.parse_args()
     LOGGER.info(f"args: {args}")
@@ -188,4 +125,11 @@ def main(args):
 
 if __name__ == '__main__':
     args = parse_opt()
+    if args.v7:
+        print(f"Use TensorRT 7.x.x.x")
+        from py.backends.backend_tensorrt_7x import BackendTensorRT_7x as BackendTensorRT
+    else:
+        print(f"Use TensorRT 8.x.x.x")
+        from py.backends.backend_tensorrt_8x import BackendTensorRT_8x as BackendTensorRT
+
     main(args)
